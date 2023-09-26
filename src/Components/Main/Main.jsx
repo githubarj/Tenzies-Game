3412afc0c014bc300d1041c8152339102cd197cd
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-09-26 21:53:51 +0300

    sacafoolding done

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import "./main.css"
+function Main() {
+  return (
+    <div className="main-container" >
+      <div className="main-center">
+
+      </div>
+    </div>
+  )
+}
+
+export default Main
